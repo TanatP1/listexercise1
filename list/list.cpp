c9da148ee3fabbbd20d28e3fab4551a8bab6d857
TanatP1 <TanatP1@bitbucket.org>
2023-06-20 17:19:40 +0700

    initial

--- list/list.cpp
+++ list/list.cpp
@@ -19,8 +19,21 @@ void List::pushToHead(char el)
 	}
 }
 void List::pushToTail(char el)
-{
-	//TO DO!
+{	
+	// 1ST CASE NOTHING IN THE LIST
+	if (head == 0) 
+	{
+		Node *tmp = new Node(el, NULL); // NODE POINTED TO NULL
+		tail = head = tmp;				// SET TAIL AND HEAD EQUAL TO TMP
+	}
+
+	// 2ND CASE MULTIPLE ELEMENTS IN THE LIST
+	else
+	{
+		Node *tmp = new Node(el, NULL); // NODE POINTED TO NULL
+		tail->next = tmp; 
+		tail = tail->next; 
+	}
 }
 char List::popHead()
 {
@@ -40,15 +53,52 @@ char List::popHead()
 char List::popTail()
 {
 	// TO DO!
-	return NULL;
+	Node *tmp = head;
+
+	if (head == 0) // 1ST CASE NOTHING IN THE LIST
+	{
+		std::cout << "The list is empty" << std::endl;
+	}
+	else if (head != tail) // MULTIPLE ELEMENTS IN THE LIST
+	{
+		while (tmp->next != tail) // SEACH FOR A NODE BEFORE TAIL NODE
+		{
+			tmp = tmp->next; // MAKE TMP POINTER POINTS TO THE NEXT ELEMENT
+		}
+		tail = tmp; // MOVE TAIL TO TMP POINTER
+		delete tail->next;
+		tail->next = NULL; // SET THE TAIL NODE TO POINT TOWARD NULL
+	}
+	else if (head == tail) // 3RD CASE A SINGLE ELEMENT IS IN THE LIST
+	{
+		delete tmp;
+		head = tail = NULL;
+		std::cout << "There is zero element in the list" << std::endl;
+	}
+	
+	return 0;
 }
 bool List::search(char el)
-{
+{	
 	// TO DO! (Function to return True or False depending if a character is in the list.
-	return NULL;
+	Node *tmp = head;
+	while (tmp != NULL) // LOOP THE ENTIRE LIST
+	{
+		if (tmp->data == el) // IF THE INPUT = ELEMENT
+		{
+			return true;
+		}
+		else
+		{
+			tmp = tmp->next; // MOVE TMP POINTER TO THE NEXT ELEMENT
+		}
+	}
+	return false;
+	
 }
 void List::reverse()
 {
+
 	// TO DO! (Function is to reverse the order of elements in the list.
 }
 void List::print()
--- list/program.cpp
+++ list/program.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
 #include "list.h"
+#include "list.cpp"
 using namespace std;
 
-void main()
+int main()
 {
 	//Sample Code
-	List mylist;
-	mylist.pushToHead('k');
-	mylist.pushToHead('e');
-	mylist.pushToHead('n');
-	mylist.print();
+
+	// FOR HEADPUSH
+	List mylist1;
+	mylist1.pushToHead('k');
+	mylist1.pushToHead('e');
+	mylist1.pushToHead('n');
+	mylist1.print(); // OUTPUT LIST1 : nek
+
+	cout << endl;
+
+    // FOR TAIL PUSH
+	List mylist2;
+	mylist2.pushToTail('k');
+	mylist2.pushToTail('e');
+	mylist2.pushToTail('n');
+	mylist2.print(); // OUTPUT LIST2 : ken
+
+	cout << endl;
+
+	// POPTAIL : POP K FROM LIST1
+	mylist1.popTail();
+	mylist1.print(); // THE OUTPUT GONNA BE LIKE ne
+
+	cout << endl;
+
+
+	//SEARCHING THE PARTICULAR ELEMENT IN LIST2
+	char element; // DECLARE THE VARIABLE FOR BEING TESTER
+	cout << "Check ELEMENTS in the list 2 : "; // PROMPT USER TO INPUT THE ELEMENT THAT WANT TO SEARCH
+	cin >> element;
+
+	if (mylist2.search(element)) // USIND SEARCHING FUNCTION FOR LIST2
+    {
+    	cout << element << " is in list 2" << endl; // IF FOUND
+    }
+    else
+    {
+        cout << element << " is not in list 2" << endl; // IF NOT FOUND
+    }
+
 
 	//TO DO! Write a program that tests your list library - the code should take characters, push them onto a list, 
 	//- then reverse the list to see if it is a palindrome!
 	
+	return 0;
 }
